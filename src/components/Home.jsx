7ff05b594127638e950b70942f6daeeeb98f8ad3
Roberto_94 <robertoverdeciasanchez@gmail.com>
2024-04-19 20:28:07 -0400

    Iconos para los botones de creación de cuenta

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 //import MembersForm from './MembersForm';
 import { Link } from "react-router-dom";
 import { useMembers } from "../context/Context";
-
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 export default function Home() {
 
   const { getTrainers } = useMembers();
@@ -15,13 +15,13 @@ export default function Home() {
   return (
     <div style={{ display: "flex", justifyContent: "space-between", gap: 10 }}>
       <Link to="/new_member" style={{ width: "50%", color: "white", textDecoration: "none" }}>
-        <Button variant="contained" style={{ width: "100%", background: "#356dac" }}>
-          Miembro
+        <Button variant="contained" style={{ width: "100%", display: "flex", justifyContent: "space-evenly", background: "#356dac" }}>
+          <PersonAddIcon /> Miembro
         </Button>
       </Link>
       <Link to="/new_trainer" style={{ width: "50%", color: "white", textDecoration: "none" }}>
-        <Button variant="contained" style={{ width: "100%", background: "#356dac" }}>
-          Entrenador
+        <Button variant="contained" style={{ width: "100%", display: "flex", justifyContent: "space-evenly", background: "#356dac" }}>
+          <PersonAddIcon /> Entrenador
         </Button>
       </Link>
     </div>
