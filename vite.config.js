f0ecffb58e2ac16e93a9e5fc06ec7491680a55a7
Roberto_94 <robertoverdeciasanchez@gmail.com>
2024-07-08 20:12:50 -0400

    Cambiando registerType de PWA

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       manifest: {
         name: 'GYManager',
         short_name: 'GYManager',
