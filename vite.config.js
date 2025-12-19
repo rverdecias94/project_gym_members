45aca89307a1db393e5dd3f88d73f68f16064dfc
Roberto_94 <robertoverdeciasanchez@gmail.com>
2025-12-19 14:49:26 -0500

    Incrementar el límite de la cache del PWA a 5MB
    El limite previo de la cache era muy restrictivo para algunos assets. Se incrementa a 5 MB el guardado de ficheros grandes para ser cacheados correctamente por el Service Worker.

--- vite.config.js
+++ vite.config.js
@@ -40,6 +40,9 @@ export default defineConfig({
           },
         ],
       },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5 MB
+      }
     }),
   ],
   define: {
